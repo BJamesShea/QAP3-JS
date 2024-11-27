26c6a76ab265f49bddd5b95c995b18c21ae49700
Brandon Shea <brandon.shea1994@icloud.com>
2024-11-27 18:01:18 -0330

    try catch for hashing password and creating + pushing new user

--- index.js
+++ index.js
@@ -53,16 +53,35 @@ app.get("/signup", (request, response) => {
 });
 
 // POST /signup - Allows a user to signup
-app.post("/signup", (request, response) => {
+app.post("/signup", async (request, response) => {
   const { email, username, password } = request.body;
 
   if (!email || !username || !password) {
-    return response.render("signup", { error: "All fields required!" });
+    return response.render("signup", { error: "All fields are required." });
   }
 
   const emailInUse = USERS.some((user) => user.email === email);
   if (emailInUse) {
-    return response.render("signup", { error: "Email already registered!" });
+    return response.render("signup", { error: "Email is already registered." });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS); //saltrounds is funny I feel like i'm in a C tier Angelina Jolie action movie. Saltrounds starring Armie Hammer as the saltman (because he eats people) (you put salt on meat) (im hilarious behave)
+
+    const newUser = {
+      id: USERS.length + 1,
+      username,
+      email,
+      password: hashedPassword,
+      role: "user",
+    };
+
+    USERS.push(newUser);
+
+    response.redirect("/login");
+  } catch (error) {
+    console.log("Error during signup", error);
+    response.render("signup", { error: "An erorr occured. Try again" });
   }
 });
 
